aab4f7f3a41835890582d7203b229d2ad0ffc6d8
slawton <seanlawton3@gmai.com>
2022-06-04 01:59:04 +0000

    added redux slice

---
+++ features/search/search.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface SearchState {
+    searchText: string;
+}
+
+const initialState: SearchState = {
+    searchText: '',
+};
+
+const searchSlice = createSlice({
+    name: "search",
+    initialState,
+    reducers: {
+        updateSearchText(state, action: PayloadAction<string>) {
+            state.searchText = action.payload.toLowerCase();
+        }
+    }
+})
+
+export default searchSlice.reducer;
