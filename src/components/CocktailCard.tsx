9d36fbd32e76a2c9ea69c4f9538d873a879e703b
slawton <seanlawton3@gmai.com>
2022-06-21 21:43:17 +0000

    changed data type of detail card

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -7,10 +6,15 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { IDrink } from '../features/types';
 import { styled } from '@mui/material';
+import ListDetail from './CocktaiDetail/ListDetail';
 
+interface IIngredients {
+  ingredient: string,
+  measure: string,
+}
 interface ICocktail {
   cocktail: IDrink;
-  ingredients?: string[];
+  ingredients?: Array<IIngredients>;
   instructions?: string[];
 }
 
@@ -20,6 +24,7 @@ const StyledCard = styled(Card)({
     backgroundSize: '200%',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     transition: '0.6s',
+    textAlign: 'center',
     backgroundImage: 'linear-gradient(#9f7928, #8A6E2F, #bb9b49)',
     '&:hover': {
       backgroundPosition: 'right'
@@ -27,27 +32,72 @@ const StyledCard = styled(Card)({
 })
 
 export default function CocktailCard({ cocktail, ingredients, instructions }: ICocktail) {
+
+  const createDetailedCard = (ingredients: Array<IIngredients>, instructions: string[]) => {
+    return (
+      <div>
+        <StyledCard>
+          <CardMedia
+            component="img"
+            alt={cocktail?.strDrink}
+            height="140"
+            image={cocktail?.strDrinkThumb}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {cocktail?.strDrink}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {cocktail?.strIBA}
+            </Typography>
+          </CardContent>
+          <Typography variant="h5" color="text.secondary">
+              Ingredients
+            </Typography>
+          <CardContent>
+            <ListDetail details={ingredients} />
+          </CardContent>
+          <Typography variant="h5" color="text.secondary">
+              Instructions
+            </Typography>
+          <CardContent>
+            <ListDetail details={instructions} />
+          </CardContent>
+        </StyledCard>
+      </div>
+    )
+  }
+
+  const createStandardCard = () => {
+    return (
+      <div>
+        <StyledCard>
+          <CardMedia
+            component="img"
+            alt={cocktail?.strDrink}
+            height="140"
+            image={cocktail?.strDrinkThumb}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {cocktail?.strDrink}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {cocktail?.strIBA}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Ingredients</Button>
+          </CardActions>
+        </StyledCard>
+      </div>
+    )
+  }
+
   return (
-    <StyledCard>
-      <CardMedia
-        component="img"
-        alt={cocktail?.strDrink}
-        height="140"
-        image={cocktail?.strDrinkThumb}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {cocktail?.strDrink}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {cocktail?.strIBA}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Ingredients</Button>
-      </CardActions>
-    </StyledCard>
-    
+    <div>
+      {ingredients && instructions ? createDetailedCard(ingredients, instructions) : createStandardCard()}
+    </div>
   );
 }
--- src/components/CocktailDetail.tsx
+++ src/components/CocktailDetail.tsx
@@ -1,36 +1,49 @@
 
-import { Box, Checkbox, Grid, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import React, { FC, useEffect, useState } from 'react'
+import { Box } from '@mui/material';
+import { FC, useEffect, useState } from 'react'
 import { IDrink } from '../features/types'
-import ListIngredients from './CocktaiDetail/ListIngredients';
 import CocktailCard from './CocktailCard';
 interface CocktailDetailProps {
   drink: IDrink;
 }
-interface ICocktail {
-  "cocktail": IDrink;
-  "ingredients"?: string[];
-  "instructions"?: string[] 
+
+interface IIngredients {
+  ingredient: string,
+  measure: string,
 }
 
 const CocktailDetail: FC<CocktailDetailProps> = ({ drink }) => {
 
-  const [ingredients, setIngredients] = useState<string[]>([])
-  const [instructions, setInstructions] = useState<string[]>([])
+  const [ingredients, setIngredients] = useState<string[]>([]);
+  const [instructions, setInstructions] = useState<string[]>([]);
+  const [measures, setMeasures] = useState<string[]>([]);
 
   useEffect(() => {
     const parseDrink = (drink: IDrink) => {
       const ingreds = Object.fromEntries(Object.entries(drink).filter(([key, value]) => key.includes('strIngredient') && value !== null));
-      const instrucs = Object.fromEntries(Object.entries(drink).filter(([key, value]) => key.includes('strInstructions') && value !== null));
-      
+      const instrucs = Object.fromEntries(Object.entries(drink).filter(([key, value]) => key === 'strInstructions' && value !== null));
+      const measure = Object.fromEntries(Object.entries(drink).filter(([key, value]) => key.includes('strMeasure') && value !== null));
+
       setIngredients(Object.values(ingreds))
       setInstructions(Object.values(instrucs));
+      setMeasures(Object.values(measure));
     };
     parseDrink(drink);
   }, [])
 
   const displayDetailedCard = (): JSX.Element => {
-      const detail = { cocktail: drink, ingredients: ingredients, instructions: instructions }
+      let ingredientMeasures:  = []
+      const combine = () => {
+        for(let i of ingredients){
+            ingredientMeasures.push{ingredient: ingredients[i], measure: measures[i]};
+        }
+      }
+      const detail = { 
+                      cocktail: drink, 
+                      ingredients: ingredients,
+                      instructions: instructions, 
+                      measures: measures 
+                    }
       return (
         <Box
             display="flex"
